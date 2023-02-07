9683c515195d2a0daed74bd3a078e7daf52674e9
Hritac Alexandru <alexhritac@icloud.com>
2023-02-07 14:21:20 +0100

    First commit

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -182,8 +182,8 @@ demo = {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
+            suggestedMin: 10,
+            suggestedMax: 35,
             padding: 20,
             fontColor: "#9a9a9a"
           }
@@ -423,8 +423,8 @@ demo = {
 
 
 
-    var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-    var chart_data = [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100];
+    var chart_labels = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+    var chart_data = [21, 23, 25, 19, 20, 24, 18];
 
 
     var ctx = document.getElementById("chartBig1").getContext('2d');
@@ -466,20 +466,14 @@ demo = {
       myChartData.update();
     });
     $("#1").click(function() {
-      var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
+      var chart_labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+      var chart_data = [20, 24, 18, 25];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
 
-    $("#2").click(function() {
-      var chart_data = [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130];
-      var data = myChartData.config.data;
-      data.datasets[0].data = chart_data;
-      data.labels = chart_labels;
-      myChartData.update();
-    });
 
 
     var ctx = document.getElementById("CountryChart").getContext("2d");
